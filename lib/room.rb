c29a52eee2ecc24e0037dd6f4865c60d1eb27c50
Cam C <ckc.cameron@gmail.com>
2020-05-13 09:26:59 -0600

    Completes Iteration 1

--- lib/room.rb
+++ lib/room.rb
@@ -1,12 +1,29 @@
-class Rom
+require 'pry'
+class Room
+  attr_reader :category
+              :length
+              :width
 
   def initialize(category, length, width)
     @category = category
     @length = 10
     @width = width
+    @is_painted = false
+  end
+
+  def length
+    @length
   end
 
   def area
-    @length * @width
+    @length * @width.to_i
+  end
+
+  def is_painted?
+    @is_painted
+  end
+
+  def paint
+    @is_painted = true
   end
 end
